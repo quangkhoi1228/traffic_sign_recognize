39a31812205b493835624d97d4f4c3ad4524af25
khoiqt <khoiqt>
2019-11-06 01:36:10 +0700

    update

--- trafficsign/urls.py
+++ trafficsign/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
+    path('trafficinfomation/', views.trafficinfomation, name='trafficinfomation'),
     path('testapi/', views.detectTrafficSign, name='detecttrafficsign'),
     path('admin/', admin.site.urls),
+    path('my-ajax-test/', views.testcall),
+
 ]
--- trafficsign/views.py
+++ trafficsign/views.py
@@ -10,5 +10,18 @@ def index(request):
     context = {}
     return render(request, 'index.html', context=context)
 
+def trafficinfomation(request):
+    context = {}
+    return render(request, 'trafficinfomation.html', context=context)
+
 def detectTrafficSign(request):
     return JsonResponse({'data':'hihi'})
+
+def testcall(request):
+   #Get the variable text
+   text = request.POST['text']
+   #Do whatever with the input variable text
+   response = text + ":)"
+   #Send the response 
+
+   return HttpResponse(response)
