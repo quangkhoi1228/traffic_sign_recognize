43e7baa10863ab6de73ec7603d371814e498d1f8
khoiqt <khoiqt>
2019-11-06 11:29:44 +0700

    add util.py

---
+++ trafficsign/util.py
@@ -0,0 +1,7 @@
+import base64
+
+def saveBase64Image(imgstring):
+    imgdata = base64.b64decode(imgstring)
+    filename = 'some_image.jpg'  # I assume you have a way of picking unique filenames
+    with open(filename, 'wb') as f:
+        f.write(imgdata)
