15726f58ee90ee1afd5dd98a1a78216e6a3d40d1
khoiqt <khoiqt>
2019-11-08 11:28:04 +0700

    add try except for util

--- trafficsign/util.py
+++ trafficsign/util.py
@@ -11,6 +11,8 @@ import random
 from tensorflow.keras.models import load_model
 from PIL import Image
 import os
+import sys
+
 
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
@@ -51,47 +53,54 @@ def imshowgrayx(img, title='BD2L'):
     plt.show()
 
 def cropAndDetectTrafficSign(context):
-    currentPythonFilePath = os.getcwd()
-    # modelUrl = currentPythonFilePath+'/static/model/model.h5'
-    modelUrl = currentPythonFilePath+'/static/model/new_model.h5'
-    url = currentPythonFilePath + context['url']
+    try:
+        currentPythonFilePath = os.getcwd()
+        # modelUrl = currentPythonFilePath+'/static/model/model.h5'
+        modelUrl = currentPythonFilePath+'/static/model/new_model.h5'
+        url = currentPythonFilePath + context['url']
 
-    imageType = url.split('.')[1]
-    tds_img = 'lastimage.'+imageType
+        imageType = url.split('.')[1]
+        tds_img = 'lastimage.'+imageType
 
-    img = imreadx(url, tds_img)
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    mask_r1 = cv2.inRange(hsv, (0, 100, 100), (10, 255, 255))
-    mask_r2 = cv2.inRange(hsv, (160, 100, 100), (180, 255, 255))
-    mask_r = cv2.bitwise_or(mask_r1, mask_r2)
-    target = cv2.bitwise_and(img, img, mask=mask_r)
-    gblur = cv2.GaussianBlur(mask_r, (9, 9), 0)
-    edge_img = cv2.Canny(gblur, 30, 150)
-    img2 = img.copy()
-    itmp, cnts, hierarchy = cv2.findContours(
-        edge_img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(img2, cnts, -1, (0, 255, 0), 2)
-    img2 = img.copy()
-    for cnt in cnts:
-        area = cv2.contourArea(cnt)
-        if(area < 1000):
-            continue
-        ellipse = cv2.fitEllipse(cnt)
-        cv2.ellipse(img2, ellipse, (0, 255, 0), 2)
-        x, y, w, h = cv2.boundingRect(cnt)
-        a, b, c, d = x, y, w, h
-        cv2.rectangle(img2, (x, y), (x+w, y+h), (0, 255, 0), 3)
-    crop = img[b:b+d, a:a+c]
-
-    model = load_model(modelUrl)
-    data = []
-    image_from_array = Image.fromarray(crop, 'RGB')
-    crop = image_from_array.resize((30, 30))
-    data.append(np.array(crop))
-    X_test = np.array(data)
-    X_test = X_test.astype('float32')/255
-    prediction = model.predict_classes(X_test)
-    return prediction
+        img = imreadx(url, tds_img)
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        mask_r1 = cv2.inRange(hsv, (0, 100, 100), (10, 255, 255))
+        mask_r2 = cv2.inRange(hsv, (160, 100, 100), (180, 255, 255))
+        mask_r = cv2.bitwise_or(mask_r1, mask_r2)
+        target = cv2.bitwise_and(img, img, mask=mask_r)
+        gblur = cv2.GaussianBlur(mask_r, (9, 9), 0)
+        edge_img = cv2.Canny(gblur, 30, 150)
+        img2 = img.copy()
+        itmp, cnts, hierarchy = cv2.findContours(
+            edge_img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(img2, cnts, -1, (0, 255, 0), 2)
+        img2 = img.copy()
+        try:
+            for cnt in cnts:
+                area = cv2.contourArea(cnt)
+                if(area < 1000):
+                    continue
+                ellipse = cv2.fitEllipse(cnt)
+                cv2.ellipse(img2, ellipse, (0, 255, 0), 2)
+                x, y, w, h = cv2.boundingRect(cnt)
+                a, b, c, d = x, y, w, h
+                cv2.rectangle(img2, (x, y), (x+w, y+h), (0, 255, 0), 3)
+            crop = img[b:b+d, a:a+c]
+        except:
+            print("cannot border box")
+            crop = img
+        model = load_model(modelUrl)
+        data = []
+        image_from_array = Image.fromarray(crop, 'RGB')
+        crop = image_from_array.resize((30, 30))
+        data.append(np.array(crop))
+        X_test = np.array(data)
+        X_test = X_test.astype('float32')/255
+        prediction = model.predict_classes(X_test)
+        return prediction
+    except:
+        print("Bug when model predict")
+        return [10000]
 
 def detectTrafficSign(request):
     context = uploadFile(request)
