df22e002697404196d4d746c20dc330d6d49bb73
khoiqt <khoiqt>
2019-11-15 08:37:39 +0700

    init commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn trafficsignrecognize.wsgi --log-file -
+
--- trafficsign/settings.py
+++ trafficsign/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hs$n1l0^e7p0+!ttk4r*2d)*9uq60(8l_!s+!x&pj4@&qjr9+i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.43.10', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.35.32', 'localhost', '127.0.0.1']
 
 
 # Application definition
